39b4d730a1e4959e74b378d2327b4998c6e04901
ArnaudLouisy <louisy.arnaud@gmail.com>
2022-03-17 22:22:37 +0100

    Feat : ajout methodes : ajouter tache, afficher tache, supprimer tache + lancement aplli

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import appli.TodoList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] var0) {
-        TodoList todo = new TodoList(); 
+        TodoList todo = new TodoList();
     }
 
     }
--- src/appli/TodoList.java
+++ src/appli/TodoList.java
@@ -1,8 +1,12 @@
 package appli;
 
 import java.util.ArrayList;
+import  java.util.Scanner;
 public class TodoList {
+    Scanner sc = new Scanner(System.in);
     private  ArrayList<String> tache;
+    private TodoList listeTache;
+    private boolean enMarche;
 
     public TodoList() {
         this.tache = new ArrayList<String>(tache);
@@ -24,17 +28,56 @@ public class TodoList {
         this.tache.add(var1);
     }
 
-    public boolean retirer_tache(int var1) {
-        if (var1 < this.tache.size() + 1) {
-            int var2 = this.tache.size();
-            this.tache.remove(var1 - 1);
-            return var2 != var2 - 1;
+    public void saisirTache() {
+        System.out.println("Veuillez saisir une tache ...");
+        String var1 = this.sc.nextLine();
+        this.listeTache.ajouter_tache(var1);
+        System.out.println("Tache ajoutee !");
+    }
+    public void afficheListe() {
+        System.out.println(this.listeTache.toString() + "\n");
+    }
+
+    public void supprimerTache() {
+        this.afficheListe();
+        System.out.println("Veuillez saisir le numero d'une ...");
+        int var1 = this.sc.nextInt();
+        if (this.listeTache.retirer_tache(var1)) {
+            System.out.println("suppression effectuer ");
         } else {
-            System.out.println("Numero pas dans la liste");
-            return false;
+            System.out.println("erreur de supression");
         }
     }
+
+    private boolean retirer_tache(int var1) {
+        return false;
+    }
+    public void lancerAppli() {
+        System.out.println("Bienvenu sur le logiciel !\n");
+        Scanner var2 = new Scanner(System.in);
+
+        while(this.enMarche) {
+            System.out.println("Que voulez vou faire\n 1 : ajouter une tache\n 2 : retirer une tache\n 3 : afficher la liste de tache\n 4 : fermer le logiciel");
+            int var1 = var2.nextInt();
+            switch(var1) {
+                case 1:
+                    this.saisirTache();
+                    break;
+                case 2:
+                    this.supprimerTache();
+                    break;
+                case 3:
+                    this.afficheListe();
+                    break;
+                default:
+                    System.out.println("Choix incorrect");
+
 }
+        }
+
+    }
+    }
+
 
 
 

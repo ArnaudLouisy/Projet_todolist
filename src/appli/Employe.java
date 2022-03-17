b4b74ad2ded2ff99abdbcc6c262802ec2da5296d
TCHIBOZO françois <totofrancois12@gmail.com>
2022-03-17 22:08:52 +0100

    ajout de la classe Enployer et metode

---
+++ src/appli/Employe.java
@@ -0,0 +1,82 @@
+package appli;
+
+import java.sql.*;
+
+public class Employe {
+    private String nom;
+    private String prenom;
+    private String username;
+    private String service;
+    private String fonction;
+    private String mot_de_passe;
+    private boolean estConnecte = false;
+
+    public Employe(String nom, String prenom, String username, String service, String fonction, String mot_de_passe) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.username = username;
+        this.service = service;
+        this.fonction = fonction;
+        this.mot_de_passe = mot_de_passe;
+    }
+
+    public void connexion() throws SQLException, SQLException {
+
+        Connection cnx = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/user?serverTimezone=UTC",
+                "root",
+                ""
+        );
+
+        PreparedStatement maRequete = cnx.prepareStatement("Select * from user where username=? and mot_de_passe=? ");
+        maRequete.setString(1,username);
+        maRequete.setString(2,mot_de_passe);
+
+        ResultSet mesResultas = maRequete.executeQuery();
+
+        while (estConnecte=false) {
+            if (mesResultas.next()) {
+                System.out.println("Vous etes connecté");
+                estConnecte = true;
+            } else {
+                System.out.println("loging ou mot de passe incorrecte veillez ");
+                estConnecte = false;
+            }
+        }
+    }
+
+
+
+    public void inscrire() throws SQLException {
+
+        Connection cnx = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/user?serverTimezone=UTC",
+                "root",
+                ""
+        );
+
+        PreparedStatement maRequeteVerifie = cnx.prepareStatement("Select * from user where username=? and mot_de_passe=? ");
+        maRequeteVerifie.setString(1,username);
+        maRequeteVerifie.setString(2,mot_de_passe);
+
+        ResultSet mesResultas = maRequeteVerifie.executeQuery();
+
+        if (mesResultas.next()){
+            System.out.println("Un compte existe déjà au nom de : "+this.nom+" "+this.prenom+".");
+        }
+
+        else {
+            PreparedStatement maRequete = cnx.prepareStatement("Insert into user(nom,prenom,username,service,fonction,mot_de_passe) VALUES (?,?,?,?,?,?)");
+            maRequete.setString(1,this.nom);
+            maRequete.setString(2,this.prenom);
+            maRequete.setString(3,this.username);
+            maRequete.setString(4,this.service);
+            maRequete.setString(5,this.fonction);
+            maRequete.setString(6,this.mot_de_passe);
+
+            ResultSet mesResultat = maRequete.executeQuery();
+        }
+    }
+}
+
+
